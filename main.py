8fc68e96e322a279e94ee67c007f5b0c60d03b36
tchucknoia <tiagolachman@gmail.com>
2020-10-01 17:45:38 -0300

    Feito alterações para links diferentes

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import platform
 import os
-import sys
 import time
 
 def ler_txt(caminho):
     dados = open(caminho, 'r')
     nome = ''
     senha = ''
+    links = []
     linhas = dados.readlines()
+    #print(linhas)
     for linha in linhas:
-        if linha.find('nome') != -1:
-            nome = linha.strip('nome:').rstrip()
-        if linha.find('senha') != -1:
-            senha = linha.strip('senha')
+        if linha.find('nome BOT LOGIN') != -1:
+            nome = linha.strip('nome BOT LOGIN:').rstrip()
+    for linha in linhas:
+        if linha.find('senha BOT LOGIN') != -1:
+            senha = linha.strip('senha BOT LOGIN')
             senha = senha.strip(':').rstrip()
+    for linha in linhas:
+        if linha.find('links BOT LOGIN') != -1:
+            aux = ''
+            for letra in linha.strip('links BOT LOGIN:'):
+                if letra == ',':
+                    links.append(aux)
+                    aux = ''
+                aux = aux + letra
+            for x in range(len(links)):
+                links[x] = links[x].strip(',').strip('[').strip(']').strip(' ')
+                if links[x].find('http://') == -1 or links[x].find('https://') == -1:
+                    raise ValueError('links do \'nome_senha.txt\' inadequados , sem \'http\', etc...')
+        if nome == '' or senha == '' or links == '':
+            raise ValueError("valores do nome_senha.txt vazios")
     dados.close()
-    return {'email': nome, 'senha': senha}
+    return {'email': nome, 'senha': senha, 'links': links}
 
 if __name__ == '__main__':
     from selenium import webdriver
@@ -27,13 +43,14 @@ if __name__ == '__main__':
         diretorio_WebDriver = diretorio + '\\WebDrivers\\chromedriver.exe'
         diretorio_txt = diretorio + '\\nome_senha.txt'
 
-
     dados = ler_txt(diretorio_txt)
     print('Salve, sou Jubiscreison seu BOT para logar no Gambolao.net O.O')
     print('Peguei alguns dados que estava no arquivo nome_senha.txt:')
-    print(f"email:{dados.get('email')}\nsenha:{dados.get('senha')}")
+    print(f"email:{dados.get('email')}\nlinks:{dados.get('links')}")
     print('E agora to abrindo o Chrome...')
+
     browser = webdriver.Chrome(diretorio_WebDriver)
+
     browser.get('http://gambolao.net/main.php')
     username_textbox = browser.find_element_by_name('username')
     username_textbox.send_keys(dados.get('email'))
@@ -42,14 +59,10 @@ if __name__ == '__main__':
 
     login_button = browser.find_element_by_xpath("/html/body/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/form[1]/table[1]/tbody[1]/tr[2]/td[2]/input[2]")
     login_button.submit()
-
-    time.sleep(3)
-    browser.get('https://gambolao.net/federacao.php?nome=Cazaquistao&mural=sim')
-    time.sleep(3)
-    browser.get('https://gambolao.net/federacao.php?mural=sim&pag=2&nome=Cazaquistao')
-    time.sleep(3)
-    browser.get('https://gambolao.net/federacao.php?mural=sim&pag=3&nome=Cazaquistao')
-    time.sleep(3)
+    print(range(len(dados.get('links'))))
+    for cont in range(len(dados.get('links'))):
+        time.sleep(3)
+        browser.get(dados.get('links')[cont])
 
     browser.quit()
-    sys.exit()
+    exit()
